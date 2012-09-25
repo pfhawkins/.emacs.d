bb8197a4e16aaa9638365dfdb55b64b9dd938450
P. F. Hawkins <pf@pfhawkins.com>
2012-09-25 17:08:26 -0400

    font size, wombat theme, but accidentally regained toolbar-mode

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(tool-bar-mode nil))
+ '(custom-enabled-themes (quote (wombat)))
+ '(custom-safe-themes (quote ("3ccdb31bb1b09fb2d175f75c4bc2d06bde7ae3498176b7b749bd1b94b48d16b9" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Inconsolata")))))
