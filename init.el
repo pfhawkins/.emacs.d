dee5d1703c8f0216bd8fc6ef7a1ae2d11bb998a4
P. F. Hawkins <pf@pfhawkins.com>
2012-09-26 10:52:16 -0400

    hardcoding cwd due to where homebrew puts emacs. TERRIBLE KLUDGE

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (setq pfh-emacs-config-dir
       (file-name-directory pfh-emacs-init-file))
 
+(setq default-directory "/Users/pf/")
+
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" pfh-emacs-config-dir))
 (load custom-file)
